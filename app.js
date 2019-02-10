ab3024871512ba97c477b2e65141beafbe08724c
Yvonne Liu <yvonne61m@gmail.com>
2019-02-10 18:43:27 +0000

    modified app.js

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost/yelp_camp_v12");
+//mongoose.connect("mongodb://localhost/yelp_camp_v12");
+mongoose.connect("mongodb+srv://yvonne:371158@yelpcamp-7tl8t.mongodb.net/test?retryWrites=true");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
